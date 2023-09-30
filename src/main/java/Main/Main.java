63fa0abee83426e90630e9555762f7c1701754c9
zhizhongchen <markson.chen@mail.utoronto.ca>
2023-09-29 22:02:40 -0400

    Updated starter files for Activity: Exploring APIs

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+import okhttp3.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Code for Week 3 Activity: Exploring APIs
+        // Call an API using okhttp3, and output the response
+        String query = "hippocampus";
+
+
+
+    }
+}
