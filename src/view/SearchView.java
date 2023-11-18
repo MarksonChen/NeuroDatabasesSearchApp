c708b7519f8e929c60dda15f548db3e693399ed4
zhizhongchen <markson.chen@mail.utoronto.ca>
2023-11-17 19:59:29 -0500

    Finished MainFrame & Search View - hooked everything together

--- src/view/SearchView.java
+++ src/view/SearchView.java
@@ -6,6 +6,7 @@ import use_case.query.query_one.QueryOneController;
 import use_case.switch_results_panel.SwitchResultsPanelController;
 import use_case.switch_view.SwitchViewController;
 import use_case.toggle_display_option.ToggleDisplayOptionController;
+import view_model.MainFrameViewModel;
 import view_model.SearchViewModel;
 import view_model.SearchViewState;
 import view.search_view_components.*;
@@ -16,9 +17,64 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.LinkedHashMap;
 
-public class SearchView extends JPanel {
+public class SearchView extends JPanel implements PropertyChangeListener {
+    final LinkedHashMap<String, ScrollResultsPanel> singleViewResultsPanelsMap;
+    // A LinkedHashMap is required, so it can be iterated in a fixed order
+    private final QueryBar queryBar;
+    private final OptionBar optionBar;
+    private final JPanel resultsPanel;
+    private final CardLayout cardLayout;
+    final SearchViewModel searchViewModel;
 
-    public SearchView(SearchViewModel searchViewModel, SwitchViewController switchViewController, SwitchResultsPanelController switchResultsPanelController, QueryAllController queryAllController, QueryOneController queryOneController, ToggleDisplayOptionController toggleDisplayOptionController) {
+    public SearchView(ScrollResultsPanel[] tabbedViewResultPanels, ScrollResultsPanel[] singleViewResultPanels,
+                      SearchViewModel searchViewModel, SwitchViewController switchViewController,
+                      SwitchResultsPanelController switchResultsPanelController, QueryAllController queryAllController,
+                      QueryOneController queryOneController, ToggleDisplayOptionController toggleDisplayOptionController) {
+        this.searchViewModel = searchViewModel;
 
+        setLayout(new BorderLayout());
+
+        queryBar = new QueryBar(this.searchViewModel, switchViewController, switchResultsPanelController, queryAllController, queryOneController);
+        optionBar = new OptionBar(toggleDisplayOptionController, searchViewModel);
+        resultsPanel = new JPanel();
+
+        cardLayout = new CardLayout();
+        resultsPanel.setLayout(cardLayout);
+        resultsPanel.setBackground(MainFrameViewModel.BACKGROUND_COLOR);
+        resultsPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 20, 20));
+
+        singleViewResultsPanelsMap = new LinkedHashMap<>();
+        for (int i = 0; i < tabbedViewResultPanels.length; i++) {
+            String databaseName = Database.get(i).name();
+            singleViewResultsPanelsMap.put(databaseName, singleViewResultPanels[i]);
+            resultsPanel.add(singleViewResultPanels[i], databaseName);
+        }
+
+        JTabbedPane tabbedView = new JTabbedPane();
+        tabbedView.setBackground(MainFrameViewModel.BACKGROUND_COLOR);
+        for (int i = 0; i < Database.length; i++) {
+            tabbedView.addTab(Database.get(i).name(), tabbedViewResultPanels[i]);
+        }
+
+        resultsPanel.add(tabbedView, SearchViewModel.ALL_DATABASES);
+
+        // Default to Tabbed View
+        cardLayout.show(resultsPanel, SearchViewModel.ALL_DATABASES);
+
+        add(queryBar, BorderLayout.NORTH);
+        add(optionBar, BorderLayout.WEST);
+        add(resultsPanel, BorderLayout.CENTER);
+
+        searchViewModel.addPropertyChangeListener(this);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        SearchViewState state = searchViewModel.getState();
+        switch (evt.getPropertyName()){
+            // TODO: ReuseHistoryQuery; SwitchResultsPanel; refreshing; displaying errors
+        }
+        revalidate();
+        repaint();
     }
 }
