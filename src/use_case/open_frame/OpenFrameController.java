b069fac0791ca3050fd8078fe7766bcca4a549eb
Felix Peng <felixpeng395099@gmail.com>
2023-11-20 18:53:07 -0500

    Controller and Interactor done

--- src/use_case/open_frame/OpenFrameController.java
+++ src/use_case/open_frame/OpenFrameController.java
@@ -1,10 +1,13 @@
 package use_case.open_frame;
 
-import use_case.open_frame.OpenFrameInputBoundary;
-
 public class OpenFrameController {
+    private final OpenFrameInputBoundary openFrmeInteractor;
+
+    public OpenFrameController(OpenFrameInputBoundary openFrmeInteractor) {
+        this.openFrmeInteractor = openFrmeInteractor;
+    }
 
     public void execute(String viewName) {
-
+        openFrmeInteractor.execute(viewName);
     }
 }
