69918e19d6e1f4ae63defb65eeb338c457272eb9
Zijie Ma <zijie.ma@mail.utoronto>
2023-11-20 17:32:37 -0500

    Finish with Query Part, Star/History feature may need to add code here.

--- src/use_case/query/query_one/QueryOneInteractor.java
+++ src/use_case/query/query_one/QueryOneInteractor.java
@@ -10,10 +10,35 @@ import java.io.IOException;
 import java.util.List;
 
 public class QueryOneInteractor implements QueryOneInputBoundary{
+    private final QueryDataAccessInterface queryDAO;
+    private final QueryOneOutputBoundary queryPresent;
 
+    public QueryOneInteractor(QueryOneOutputBoundary queryPresent, QueryDataAccessInterface queryDAO) {
+        this.queryDAO = queryDAO;
+        this.queryPresent = queryPresent;
+    }
+
+    /**
+     * Send back result from one specific external database.
+     *
+     * @param inputData The info that needed to start a query, both API needed and program needed.
+     */
 
     @Override
     public void execute(QueryOneInputData inputData) {
-
+        Query query = new Query(inputData.getKeywords());
+        if (query.getKeywords().trim().isEmpty()){
+            queryPresent.prepareFailView("Error: Cannot search with empty query");
+            return;
+        }
+        // Star & History may appear here
+        try {
+            List<FetchedData> fetchedData = queryDAO.queryOne(inputData.getDatabase(), query, inputData.getResultPerPage(), inputData.getPage());
+            QueryOneOutputData Output = new QueryOneOutputData();
+            queryPresent.prepareSuccessView(Output);
+        }
+        catch (IOException e){
+            queryPresent.prepareFailView("Error: Happened while fetching query on \""+ inputData.getKeywords() + "\"");
+        }
     }
 }
