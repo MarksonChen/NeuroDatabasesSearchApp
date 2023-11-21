ccbe578d18fd1442607b458371cdcbecec62699d
Ziheng-lin1030 <tzuheng.lin@mail.utoronto.ca>
2023-11-20 19:38:01 -0500

    Implement ScrollResultsPanelState and ScrollResultsPanelState.getFetchedDataList and ScrollResultsPanelState.toggleStarOfData.

--- src/view_model/ScrollResultsPanelState.java
+++ src/view_model/ScrollResultsPanelState.java
@@ -7,4 +7,17 @@ import java.util.List;
 
 public class ScrollResultsPanelState {
 
+    private List<FetchedData> fetchedDataList = new ArrayList<>();
+    private List<Boolean> dataIsStarredList = new ArrayList<>();
+
+
+    public List<FetchedData> getFetchedDataList() {
+        return fetchedDataList;
+    }
+
+    public void toggleStarOfData(FetchedData data) {
+        int index = fetchedDataList.indexOf(data);
+        dataIsStarredList.set(index, !dataIsStarredList.get(index));
+    }
+
 }
