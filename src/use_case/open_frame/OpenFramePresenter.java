5bc93f81baa8942b7e5cb7148f4e7af1843dd5cb
Felix Peng <felixpeng395099@gmail.com>
2023-11-22 17:00:48 -0500

    Finished openFramePresenter and modigied reletive view model

--- src/use_case/open_frame/OpenFramePresenter.java
+++ src/use_case/open_frame/OpenFramePresenter.java
@@ -1,11 +1,32 @@
 package use_case.open_frame;
 
 import view_model.FrameManagerModel;
+import view_model.HistoryViewModel;
 import view_model.StarredViewModel;
 
+import java.util.Objects;
+
 public class OpenFramePresenter implements OpenFrameOutputBoundary {
+
+    private final FrameManagerModel frameManagerModel;
+    private final StarredViewModel starredViewModel;
+    private final HistoryViewModel historyViewModel;
+
+    public OpenFramePresenter(FrameManagerModel frameManagerModel, StarredViewModel starredViewModel, HistoryViewModel historyViewModel) {
+        this.frameManagerModel = frameManagerModel;
+        this.starredViewModel = starredViewModel;
+        this.historyViewModel = historyViewModel;
+    }
+
     @Override
     public void openFrame(String viewName) {
+        frameManagerModel.setActiveView(viewName);
+        frameManagerModel.firePropertyChanged("OPEN");
+        if(Objects.equals(viewName, StarredViewModel.VIEW_NAME))
+            starredViewModel.firePropertyChanged(StarredViewModel.REFRESH);
+        else if (Objects.equals(viewName, HistoryViewModel.VIEW_NAME)) {
+            historyViewModel.firePropertyChanged(HistoryViewModel.REFRESH);
+        }
 
     }
 }
--- src/view_model/FrameManagerModel.java
+++ src/view_model/FrameManagerModel.java
@@ -5,11 +5,24 @@ import java.beans.PropertyChangeSupport;
 
 public class FrameManagerModel implements ObserverViewModel{
 
+    private String activeView;
+
+    public String getActiveView() {
+        return activeView;
+    }
+
+    public void setActiveView(String activeView){
+        String oldView = this.activeView;
+        this.activeView = activeView;
+    };
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    @Override
     public void firePropertyChanged(String propertyName) {
-        // TODO: OpenFrame use case
+        support.firePropertyChange(propertyName, null, activeView);
     }
+
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
