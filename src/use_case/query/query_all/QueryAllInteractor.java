d859789a33580b7b88ab4806408c954392f873e8
Zijie Ma <zijie.ma@mail.utoronto>
2023-11-27 17:42:49 -0500

    Finish with feature QueryAll

--- src/use_case/query/query_all/QueryAllInteractor.java
+++ src/use_case/query/query_all/QueryAllInteractor.java
@@ -11,8 +11,33 @@ import java.util.List;
 
 public class QueryAllInteractor implements QueryAllInputBoundary {
 
+    private final QueryAllOutputBoundary queryAllPresenter;
+    private final QueryDataAccessInterface queryDAO;
+
+    public QueryAllInteractor(QueryAllOutputBoundary queryAllOutputBoundary, QueryDataAccessInterface queryDAO) {
+        this.queryAllPresenter = queryAllOutputBoundary;
+        this.queryDAO = queryDAO;
+    }
+
     @Override
     public void execute(QueryAllInputData inputData) {
+        String keywords = inputData.getKeywords();
+        Query query = new Query(keywords);
+        if (query.getKeywords().trim().isEmpty()){
+            queryAllPresenter.prepareFailView("Cannot search with empty query, please try again");
+            return;
+        }
+        // Star & History may appear here
+        List<Boolean>[] starredStateList = null;
+        List<Query> historyQueryList = null;
+        try {
+            List<FetchedData>[] fetchedData = queryDAO.queryAll(query, inputData.getResultPerPage(), 1);
+            int[] queryAllTotalResults = queryDAO.getQueryAllTotalResults();
 
+            QueryAllOutputData outputData = new QueryAllOutputData(keywords, fetchedData, starredStateList, historyQueryList, queryAllTotalResults);
+            queryAllPresenter.prepareSuccessView(outputData);
+        } catch (IOException e) {
+            queryAllPresenter.prepareFailView("An error occured while fetching query for \"" + keywords + "\"");
+        }
     }
 }
--- src/use_case/query/query_all/QueryAllPresenter.java
+++ src/use_case/query/query_all/QueryAllPresenter.java
@@ -10,14 +10,33 @@ import view_model.SearchViewModel;
 import java.util.List;
 
 public class QueryAllPresenter implements QueryAllOutputBoundary {
+    private final SearchViewModel searchViewModel;
+    private final ScrollResultsPanelModel[] resultsPanelModels;
+
+    public QueryAllPresenter(SearchViewModel searchViewModel, ScrollResultsPanelModel[] resultsPanelModels) {
+        this.searchViewModel = searchViewModel;
+        this.resultsPanelModels = resultsPanelModels;
+    }
 
     @Override
     public void prepareSuccessView(QueryAllOutputData outputData) {
-
+        List<FetchedData>[] fetchedDataListArr = outputData.getFetchedData();
+        List<Boolean>[] starredStateListArr = outputData.getStarredStateListArr();
+        int[] totalResultsList = outputData.getQueryAllTotalResults();
+        for (int i = 0; i < Database.length; i++) {
+            ScrollResultsPanelState state = resultsPanelModels[i].getState();
+            state.setFetchedDataList(fetchedDataListArr[i]);
+            state.setTotalResults(totalResultsList[i]);
+            state.setCurrentPage(1);
+            state.setLastQuery(outputData.getQueryKeywords());
+            state.setDataIsStarredList(starredStateListArr[i]);
+            resultsPanelModels[i].firePropertyChanged(ScrollResultsPanelModel.REFRESH_ALL);
+        }
     }
 
     @Override
     public void prepareFailView(String errorMessage) {
-
+        searchViewModel.getState().setErrorMessage(errorMessage);
+        searchViewModel.firePropertyChanged(SearchViewModel.ERROR);
     }
 }
