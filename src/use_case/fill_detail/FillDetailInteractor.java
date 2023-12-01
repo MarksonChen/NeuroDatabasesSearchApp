45d1bbfc9fb21e7d8db1346884e04ab27e947442
Dennis-Ding1 <dinghaochen3338333@gmail.com>
2023-12-01 00:14:37 -0500

    complete fill detail interactor

--- src/use_case/fill_detail/FillDetailInteractor.java
+++ src/use_case/fill_detail/FillDetailInteractor.java
@@ -7,8 +7,22 @@ import use_case.star.StarDataAccessInterface;
 import java.io.IOException;
 
 public class FillDetailInteractor implements FillDetailInputBoundary{
+    private final FillDetailOutputBoundary fillDetailPresenter;
+    private final QueryDataAccessInterface queryDAO;
+
+    public FillDetailInteractor(FillDetailOutputBoundary fillDetailPresenter, QueryDataAccessInterface queryDAO, StarDataAccessInterface starDAO) {
+        this.fillDetailPresenter = fillDetailPresenter;
+        this.queryDAO = queryDAO;
+
+    }
+
     @Override
     public void execute(FetchedData inputData) {
-
+        try {
+            FetchedData updatedData = queryDAO.fillDetails(inputData);
+            fillDetailPresenter.prepareSuccessView(updatedData);
+        } catch (IOException e) {
+            fillDetailPresenter.prepareFailView("Failed filling details for entry \"" + inputData.getTitle() + "\"");
+        }
     }
 }
