d6a311c74678110590e9f8c90bb539083f3687c9
Dennis-Ding1 <dinghaochen3338333@gmail.com>
2023-12-01 00:21:57 -0500

    complete FillDetailPresenter

--- src/use_case/fill_detail/FillDetailPresenter.java
+++ src/use_case/fill_detail/FillDetailPresenter.java
@@ -9,13 +9,26 @@ import view_model.StarredViewModel;
 import java.util.List;
 
 public class FillDetailPresenter implements FillDetailOutputBoundary {
+    private final ScrollResultsPanelModel[] resultsPanelModels;
+    private final MainFrameViewModel mainFrameViewModel;
+
+    public FillDetailPresenter(ScrollResultsPanelModel[] resultsPanelModels, StarredViewModel starredViewModel, MainFrameViewModel mainFrameViewModel) {
+        this.resultsPanelModels = resultsPanelModels;
+        this.mainFrameViewModel = mainFrameViewModel;
+    }
+
     @Override
     public void prepareSuccessView(FetchedData outputData) {
-
+        ScrollResultsPanelModel model = resultsPanelModels[Database.indexOf(outputData.getDatabase())];
+        List<FetchedData> stateDataList = model.getState().getFetchedDataList();
+        if (stateDataList.contains(outputData)){
+            model.firePropertyChanged(ScrollResultsPanelModel.REFRESH_DATA_INFO_PANEL);
+        }
     }
 
     @Override
     public void prepareFailView(String errorMessage) {
-
+        mainFrameViewModel.getState().setErrorMessage(errorMessage);
+        mainFrameViewModel.firePropertyChanged(MainFrameViewModel.ERROR);
     }
 }
