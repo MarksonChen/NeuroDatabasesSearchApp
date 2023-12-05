b3ae419418a492cde40a843c08e6dcf8a3a8b421
Dennis-Ding1 <dinghaochen3338333@gmail.com>
2023-12-05 16:37:10 -0500

    modify fill detail interactor and present to support change in star view simultaneously

--- src/use_case/fill_detail/FillDetailInteractor.java
+++ src/use_case/fill_detail/FillDetailInteractor.java
@@ -9,11 +9,12 @@ import java.io.IOException;
 public class FillDetailInteractor implements FillDetailInputBoundary{
     private final FillDetailOutputBoundary fillDetailPresenter;
     private final QueryDataAccessInterface queryDAO;
+    private final StarDataAccessInterface starDAO;
 
     public FillDetailInteractor(FillDetailOutputBoundary fillDetailPresenter, QueryDataAccessInterface queryDAO, StarDataAccessInterface starDAO) {
         this.fillDetailPresenter = fillDetailPresenter;
         this.queryDAO = queryDAO;
-
+        this.starDAO = starDAO;
     }
 
     @Override
@@ -23,6 +24,16 @@ public class FillDetailInteractor implements FillDetailInputBoundary{
             fillDetailPresenter.prepareSuccessView(updatedData);
         } catch (IOException e) {
             fillDetailPresenter.prepareFailView("Failed filling details for entry \"" + inputData.getTitle() + "\"");
+        }
+        if (starDAO.getStarredDataList().contains(inputData)){
+            // If starDAO also contains the same fetched data,
+            // then its data will also be filled of details because of alias.
+            // Then, we must keep the persistent file the same as the in-memory state.
+            try {
+                starDAO.saveStarredData();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
